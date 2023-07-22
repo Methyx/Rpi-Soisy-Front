3fc3cb932ce0a5f61291fbce374a9f8f48d40347
Methyx <methyx95@gmail.com>
2023-07-22 11:37:52 +0200

    background image

--- src/Home/page/Home.jsx
+++ src/Home/page/Home.jsx
@@ -2,7 +2,11 @@
 import "../style/home.css";
 
 const Home = () => {
-  return <div className="home-page">Bienvenue chez Methyx !</div>;
+  return (
+    <div className="home-page">
+      <h1>Bienvenue chez Methyx !</h1>
+    </div>
+  );
 };
 
 export default Home;
